48058a31d1b413955212f3dcb64e6115e6a49df4
ParrotMan_ <92490137+ParrotMan0128@users.noreply.github.com>
2023-03-30 17:12:26 +0900

    Add files via upload

---
+++ restaurants.py
@@ -0,0 +1,8 @@
+persons = ["Kim", "Park", "Lee", "Choi"];
+restaurants = ["Korean", "American", "French", "Chinese"]
+
+for i in persons:
+
+    for j in restaurants:
+
+        print(f"{i}이 {j} 식당을 방문");
